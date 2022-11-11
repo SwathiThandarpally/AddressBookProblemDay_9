4716c05af2ba4f746d4592df881f3eda08e47a3e
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2022-11-11 23:48:58 +0530

    Added program

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace AddressBookSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Contact addressBook = new Contact();
+            bool flag = true;
+            while (flag)
+            {
+                //Console.WriteLine("1. Create  2. Edit  3.Delete ");
+               
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        addressBook.CreateContact();
+                        break;
+                    case 2:
+                        Console.Write("Enter FirstName and LastName to edit ");
+                        string edit = Console.ReadLine();
+                        addressBook.Edit(edit);
+                        break;
+                    case 3:
+                        Console.Write("Enter FirstName and LastName to Delte ");
+                        string delete = Console.ReadLine();
+                        addressBook.DeleteContacts(delete);
+                        break;
+                    
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
